29fe9cbee10d4aef069421b136895e8916d3143d
reloader <37155445+reloadersystem@users.noreply.github.com>
2020-03-24 11:04:46 -0500

    Password

--- app/src/main/java/com/salud/medicalservices/activitys/LoginActivity.java
+++ app/src/main/java/com/salud/medicalservices/activitys/LoginActivity.java
@@ -1,16 +1,43 @@
 package com.salud.medicalservices.activitys;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.salud.medicalservices.R;
 
-public class LoginActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView txv_sendpassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txv_sendpassword = findViewById(R.id.txr_review_password);
+
+        txv_sendpassword.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+
+            case R.id.txr_review_password:
+
+                Intent intent = new Intent(LoginActivity.this, SendPasswordActivity.class);
+                startActivity(intent);
+
+
+
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/salud/medicalservices/activitys/SendPasswordActivity.java
@@ -0,0 +1,41 @@
+package com.salud.medicalservices.activitys;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.salud.medicalservices.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SendPasswordActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_enviar;
+    TextInputLayout txt_email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_password);
+
+        btn_enviar = findViewById(R.id.btn_enviar);
+        txt_email = findViewById(R.id.edt_email);
+
+        btn_enviar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.btn_enviar:
+
+                String txmail = txt_email.getEditText().getText().toString();
+
+
+                break;
+        }
+    }
+}
